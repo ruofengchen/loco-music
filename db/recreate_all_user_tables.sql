001dccddd63af96a8731a7fbfbd48456468da375
Ruofeng Chen <ruofeng.ch@gmail.com>
2015-02-19 18:27:47 -0800

    db: bug fix, insert random posts

--- db/recreate_all_user_tables.sql
+++ db/recreate_all_user_tables.sql
@@ -25,8 +25,6 @@ CREATE TABLE `comments` (
   `author_id` INT UNSIGNED NOT NULL,
   `post_id` INT UNSIGNED NOT NULL,
   `content` varchar(2048),
-  `sound_url` varchar(2048),
-  `video_url` varchar(2048),
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -36,7 +34,9 @@ DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
   `id` INT UNSIGNED NOT NULL auto_increment,
   `author_id` INT UNSIGNED NOT NULL,
-  `content` varchar(255) collate utf8_bin,
+  `content` varchar(2048),
+  `sound_url` varchar(2048),
+  `video_url` varchar(2048),
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
