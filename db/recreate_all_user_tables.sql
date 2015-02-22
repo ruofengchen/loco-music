ee32edb57e1103e8fe738bf969f676c6334d2373
Ruofeng Chen <ruofeng.ch@gmail.com>
2015-02-22 09:13:32 -0800

    newly designed db schema

--- db/recreate_all_user_tables.sql
+++ db/recreate_all_user_tables.sql
@@ -19,26 +19,68 @@ CREATE TABLE `users` (
 CREATE INDEX district_x_index ON `users` (district_x);
 CREATE INDEX district_y_index ON `users` (district_y);
 
-DROP TABLE IF EXISTS `comments`;
-CREATE TABLE `comments` (
+DROP TABLE IF EXISTS `rates`;
+CREATE TABLE `rates` (
   `id` INT UNSIGNED NOT NULL auto_increment,
   `author_id` INT UNSIGNED NOT NULL,
-  `post_id` INT UNSIGNED NOT NULL,
-  `content` varchar(2048),
+  `session_id` INT UNSIGNED NOT NULL,
+  `r0` float(24),
+  `r1` float(24),
+  `r2` float(24),
+  `r3` float(24),
+  `r4` float(24),
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
 
-DROP TABLE IF EXISTS `posts`;
-CREATE TABLE `posts` (
+DROP TABLE IF EXISTS `songs`;
+CREATE TABLE `songs` (
   `id` INT UNSIGNED NOT NULL auto_increment,
-  `author_id` INT UNSIGNED NOT NULL,
-  `content` varchar(2048),
-  `sound_url` varchar(2048),
-  `video_url` varchar(2048),
+  `title` varchar(2048) collate utf8_general_ci,
+  `artist` varchar(2048) collate utf8_general_ci,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 );
+CREATE INDEX title_index ON `songs` (title);
+
+DROP TABLE IF EXISTS `commits`;
+CREATE TABLE `commits` (
+  `id` INT UNSIGNED NOT NULL auto_increment,
+  `author_id` INT UNSIGNED NOT NULL,
+  `song_id` INT UNSIGNED NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+CREATE INDEX author_id_index ON `commits` (author_id);
+
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE `sessions` (
+  `id` INT UNSIGNED NOT NULL auto_increment,
+  `practice_id` INT UNSIGNED NOT NULL,
+  `content` varchar(2048),
+  `sound_url` varchar(2048),
+  `video_url` varchar(2048),
+  `version` INT UNSIGNED NOT NULL,
+  `r0` float(24),
+  `r1` float(24),
+  `r2` float(24),
+  `r3` float(24),
+  `r4` float(24),
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+CREATE INDEX practice_id_index ON `sessions` (practice_id);
+
+DROP TABLE IF EXISTS `reviewers`;
+CREATE TABLE `reviewers` (
+  `id` INT UNSIGNED NOT NULL auto_increment,
+  `session_id` INT UNSIGNED NOT NULL,
+  `reviewer_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`)
+);
+CREATE INDEX session_id_index ON `reviewers` (session_id);
 
